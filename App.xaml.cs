0bc6800016f0296e71bcdb0e080e4ef342700cc7
Helitop <Helitop@DESKTOP-IDTNQNB>
2026-03-10 15:48:13 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Factory_Method
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
